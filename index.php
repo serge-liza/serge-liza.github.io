f3b5fb3bad1f5559a19a49a3e4a46647d4a6d24f
serge-liza <lizaveta310896@gmail.com>
2017-01-07 13:20:00 +0700

    to Nuss

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<img src="">
+<h1></h1>
+
+<a href=""></a>
+</body>
+</html>
--- views/layouts/main.php
+++
@@ -1,5 +0,0 @@
-<h1>Its layout</h1>
-
-<?= $content ?>
-
-<h4>footer</h4>
